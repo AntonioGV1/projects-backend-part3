fa9f7bb88795de3aa43fc9c94257ba8ef1868096
jose <joseantonio.vilca04@gmail.com>
2025-02-23 15:12:15 -0500

    All right

--- index.js
+++ index.js
@@ -92,7 +92,6 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-
 app.get('/info', (request, response) => {
     const date = new Date()
     const info = `<p> Phonebook has info for ${persons.length} people</p>
